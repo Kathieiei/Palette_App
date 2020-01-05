71f309ab613ad7be7703277d8ac6244a8ca2f79e
Kathieiei <57031362+Kathieiei@users.noreply.github.com>
2020-01-05 17:39:18 +0700

    v.1

---
+++ Palette/app/src/main/java/th/ac/su/ict/thitiwan/palette/MainActivity.kt
@@ -0,0 +1,18 @@
+package th.ac.su.ict.thitiwan.palette
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
