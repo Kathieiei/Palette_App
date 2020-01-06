4e38432f90d84a8dc1ace571240ee31f54835c12
Kathieiei <57031362+Kathieiei@users.noreply.github.com>
2020-01-06 15:09:17 +0700

    kt

--- Palette/app/src/main/java/th/ac/su/ict/thitiwan/palette/MainActivity.kt
+++ Palette/app/src/main/java/th/ac/su/ict/thitiwan/palette/MainActivity.kt
@@ -12,6 +12,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//
+        var bt1 = findViewById<Button>(R.id.bt1)
+        var bt2 = findViewById<Button>(R.id.bt2)
+        var bt3 = findViewById<Button>(R.id.bt3)
+        var bt4 = findViewById<Button>(R.id.bt4)
+        var bt5 = findViewById<Button>(R.id.bt5)
+        var bt6 = findViewById<Button>(R.id.bt6)
+        var viewColors = findViewById<TextView>(R.id.viewColors)
+
+        bt1.setOnClickListener {
+            viewColors.setText("235-255-221")
+            viewColors.setBackgroundColor(Color.parseColor("#ebe1dd"))
+
+        }
+        bt2.setOnClickListener(){
+            viewColors.setText("236-203-202")
+            viewColors.setBackgroundColor(Color.parseColor("#eccbca"))
+        }
+        bt3.setOnClickListener(){
+            viewColors.setText("117-131-156")
+            viewColors.setBackgroundColor(Color.parseColor("#75839c"))
+        }
+        bt4.setOnClickListener(){
+            viewColors.setText("153-169-191")
+            viewColors.setBackgroundColor(Color.parseColor("#99a9bf"))
+        }
+        bt5.setOnClickListener(){
+            viewColors.setText("195-204-216")
+            viewColors.setBackgroundColor(Color.parseColor("#c3ccd8"))
+        }
+        bt6.setOnClickListener(){
+            viewColors.setText("221-226-269")
+            viewColors.setBackgroundColor(Color.parseColor("#dee2e5"))
+        }
+
+
+
 
 
     }
